ed8cb94d8aa28011973394b6858c1974c5df95fb
Hayato Matsumuro <Hayato_Matsumuro@amber.plala.or.jp>
2017-06-11 20:50:05 +0900

    jquery 動作テスト
     ・ Php Test の文字が赤くなるようにする

---
+++ babanuki.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $("[name=test]").css("color","#f00");
+});
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="babanuki.js"></script>
 <title>Php Test</title>
 </head>
 <body>
-Php Test
+<div name="test">Php Test</div>
 </body>
 </html>
