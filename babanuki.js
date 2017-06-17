b44c1fed40f1f780c9104b56aa22d9ee9437228f
Hayato Matsumuro <Hayato_Matsumuro@amber.plala.or.jp>
2017-06-17 10:37:26 +0900

    山札を作成し、シャッフルする

--- babanuki.js
+++ babanuki.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-  $("[name=test]").css("color","#f00");
+  var $handcard = [];
+  
+  <!-- 山札 -->
+  $("#start").on("click", function(){
+    <!-- 山札 -->
+    var deck = [];
+
+    <!-- 山札にカードをセット -->
+    for(var i=0; i<53; i++) {
+      deck[i] = i;
+    }
+
+    <!-- 山札シャッフル -->
+    for(var i=0; i<150; i++) {
+      var tmp;
+      var rand1 = Math.floor( Math.random() * 53);
+      var rand2 = Math.floor( Math.random() * 53);
+
+      tmp = deck[rand1];
+      deck[rand1] = deck[rand2];
+      deck[rand2] = tmp
+    }
+
+  });
 });
