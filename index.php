f5da53eaccf0a6f7fb44ad252750d92791ea0000
Hayato Matsumuro <Hayato_Matsumuro@amber.plala.or.jp>
2017-06-17 09:33:08 +0900

    大まかなレイアウト作成

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="babanuki.js"></script>
-<title>Php Test</title>
+<title>ババ抜き</title>
 </head>
 <body>
-<div name="test">Php Test</div>
+<div id="title" style="position: absolute; left: 10px; top: 10px"><b>ババ抜き</b></div>
+<div id="message" style="position: absolute; left: 10px; top: 50px">開始を押してください。</div>
+<button id="start" style="position: absolute; left: 10px; top: 100px">開始</button>
+<br><br>
+<div id="com1" style="position: absolute; left: 10px; top: 150px">プレイヤー</div>
+<img src="trump.png" id="pic10" style="position: absolute; left: 200px; top: 150px; clip: rect(0px 51px 76px 0px);">
+<div id="com1" style="position: absolute; left: 10px; top: 250px">コンピューター1</div>
+<div id="com2" style="position: absolute; left: 10px; top: 350px">コンピューター2</div>
+<div id="com3" style="position: absolute; left: 10px; top: 450px">コンピューター3</div>
+
 </body>
 </html>
